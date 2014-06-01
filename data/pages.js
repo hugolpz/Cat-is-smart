25474b46670bcdce0c9b0018f70ca6e240bb67ed
hugolpz <hugo.lpz@gmail.com>
2014-06-01 19:05:17 +0000

    dev:css, data, & minors.

---
+++ data/pages.js
@@ -0,0 +1,7 @@
+var webpages=[
+	{ "hant":"1", "href":"/pages/search", "class":"info", "icon":"fa fa-search", "title":"Dictionnaire", "text":"The basis, a searchable electronic dictionary." },
+	{ "hant":"2", "href":"/pages/test", "class":"warning", "icon":"fa fa-bomb", "title":"Test", "text":"Tell us what do you know, so we can craft a personalized curiculum." },
+	{ "hant":"3", "href":"/pages/next", "class":"danger", "icon":"fa fa-rocket", "title":"Good to learn", "text":"According to previous knowledges, there are the good words to learn." },
+	{ "hant":"4", "href":"/pages/favorite", "class":"warning", "icon":"fa fa-star", "title":"Favoris", "text":"Gather words you wish to focus on! Most recent on the top." },
+	{ "hant":"5", "href":"/pages/learned", "class":"success", "icon":"fa fa-check", "title":"Learned", "text":"Here are all what you learnt, the most recent on the top."}
+];
